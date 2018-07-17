ff622e5c8f450e7018886ac1eba53b8a102a3135
Zhongming Chen <ZhongmingC@asos.com>
2018-07-17 22:06:25 +0100

    calculate total number of weekends between a start date and end date

---
+++ NumberOfWeekends.UnitTests/WeekendCalculatorTests.cs
@@ -0,0 +1,23 @@
+﻿using number_of_weekends;
+using NUnit.Framework;
+using System;
+using System.Globalization;
+
+namespace NumberOfWeekends.UnitTests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [TestCase("17/07/2018", "19/07/2018", 0, Description = "Both start and end date are not weekends")]
+        [TestCase("21/07/2018", "22/07/2018", 2, Description = "Both start and end date are weekends")]
+        [TestCase("20/07/2018", "24/07/2018", 2, Description = "End date is on next week while start date is on previous week")]
+        public void Should_calculate_correctly(string dateStart, string dateEnd, int expected)
+        {
+            var start = DateTime.ParseExact(dateStart, "d/M/yyyy", CultureInfo.InvariantCulture);
+            var end = DateTime.ParseExact(dateEnd, "d/M/yyyy", CultureInfo.InvariantCulture);
+            var actual = WeekendCalculator.GetTotalNUmberOfWeekends(start, end);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ number-of-weekends/WeekendCalculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace number_of_weekends
+{
+    public class WeekendCalculator
+    {
+        public static int GetTotalNUmberOfWeekends(DateTime dateTimeStart, DateTime dateTimeEnd)
+        {
+            if (dateTimeEnd < dateTimeStart)
+                throw new ArgumentException("dataTimeEnd cannot be before dateTimeStart");
+
+            var totalDays = (dateTimeEnd - dateTimeStart).Days;
+
+            var howManyWeeks = totalDays/ 7;
+
+            return howManyWeeks * 2 + CalculateRestOfDays(totalDays % 7, dateTimeStart);
+
+        }
+
+        private static int CalculateRestOfDays(int v, DateTime date)
+        {
+            if (v <= 0)  return 0;
+
+            var start = date.DayOfWeek;
+            var end = date.AddDays(v).DayOfWeek;
+
+            if (end < start)
+                return 2;
+
+            var days = 0;
+            if (start == DayOfWeek.Saturday || start == DayOfWeek.Sunday)
+                days++;
+            if (end == DayOfWeek.Saturday || end == DayOfWeek.Sunday)
+                days++;
+
+            return days;
+        }
+    }
+}
