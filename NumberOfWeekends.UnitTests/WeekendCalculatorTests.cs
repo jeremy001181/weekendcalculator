46b5736856d91f01d0ac33b3d61878ff82fee377
Zhongming Chen <ZhongmingC@asos.com>
2018-07-18 22:17:28 +0100

    test description changes

--- NumberOfWeekends.UnitTests/WeekendCalculatorTests.cs
+++ NumberOfWeekends.UnitTests/WeekendCalculatorTests.cs
@@ -8,8 +8,8 @@ namespace NumberOfWeekends.UnitTests
     [TestFixture]
     public class Class1
     {
-        [TestCase("17/07/2018", "19/07/2018", 0, Description = "Both start and end date are not weekends")]
-        [TestCase("22/07/2018", "19/08/2018", 9, Description = "5 weeks and start date on Sunday")]
+        [TestCase("17/07/2018", "19/07/2018", 0, Description = "Both start and end date are in same week, and they are not weekends")]
+        [TestCase("22/07/2018", "19/08/2018", 9, Description = "29 days and start date on Sunday")]
         [TestCase("18/07/2018", "28/07/2018", 3, Description = "End date on Saturday")]
         [TestCase("21/07/2018", "22/07/2018", 2, Description = "Both start and end date are weekends and in same week")]
         [TestCase("20/07/2018", "24/07/2018", 2, Description = "End date is on next week while start date is on previous week")]
