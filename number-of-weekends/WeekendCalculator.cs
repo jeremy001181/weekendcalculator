8cd9a74785b26d3adc33652e344ec0b8aa07718b
Zhongming Chen <ZhongmingC@asos.com>
2018-07-18 22:24:48 +0100

    added comments

--- number-of-weekends/WeekendCalculator.cs
+++ number-of-weekends/WeekendCalculator.cs
@@ -10,31 +10,23 @@ namespace number_of_weekends
                 throw new ArgumentException("dataTimeEnd cannot be before dateTimeStart");
 
             var totalDays = (dateTimeEnd - dateTimeStart).Days + 1;
-
+            // Workout how many weeks it has
             var numberOfWeeks = totalDays/ 7;
 
+            // Then move the start date forward and work out how many weekends is in the rest of days
             return numberOfWeeks * 2 + CalculateRestOfDays(dateTimeStart.AddDays(numberOfWeeks * 7), dateTimeEnd);
 
         }
 
         private static int CalculateRestOfDays(DateTime newStartDay, DateTime endDate)
         {
-        //    if (v <= 0)  return 0;
-
             var start = newStartDay.DayOfWeek;
             var end = endDate.DayOfWeek;
 
-            if (end < start)
+            if (end < start) // Saturday is 6, Sunday is 0, so if end is less than start , it definitely has weekends in between.
                 return 2;
-            else
+            else // in case of not, we just need to check whether or not the end day is landed on a weekend
                 return end == DayOfWeek.Saturday || end == DayOfWeek.Sunday ? 1 : 0;
-            //var days = 0;
-            //if (start == DayOfWeek.Saturday || start == DayOfWeek.Sunday)
-            //    days++;
-            //if (end == DayOfWeek.Saturday || end == DayOfWeek.Sunday)
-            //    days++;
-
-            //return days;
         }
     }
 }
